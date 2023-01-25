1ee38814dbbd693bbb75d26c005973eacb5ebf0c
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2023-01-25 16:48:59 -0600

    pushing p

--- Zach/Wrangle.py
+++ Zach/Wrangle.py
@@ -2,18 +2,78 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
+import numpy as np
 
-#Creating list of top publishers
+#Creating list of top 10 publishers from https://learn.g2.com/video-game-publishers
+
 AAA = ['Tencent Games','Sony Interactive','Microsoft','Activision Blizzard','Electronic Arts','Nintendo','Bandai Namco','Take-Two Interactive','Ubisoft','Square Enix']
 
+#Creating a list of top 10 indie publishers from https://ninichimusic.com/blog/16-indie-friendly-indie-game-publishers
+
+III = ['Curve Digital','Ukuza','Team 17','Devolver Digital','Indie Fund','Midnight City','Serenity Forge','Noodlecake Studios','Versus Evil','Mode 7 Games']
+
+
 def clean_steamspy(df):
+    #Turning minutes to hours
+    df.iloc[:,[9,10,11,12]] = (df.iloc[:,[9,10,11,12]] / 60)
+    # binned target
+    ninety = np.quantile(df['average_forever'], 0.90)
+    ten = np.quantile(df['average_forever'], 0.10)
+    IQR = ninety - ten
+    bins = [0,1.565,75.253,195.97,1000]
+    labels = ['rarely_played','moderately_played','heavily_played','most_played']
+    df['binned_hours'] = pd.cut(df['average_forever'], bins=bins, labels=labels)
+    #formatting pub and dev strings
+    df.publisher = df.publisher.str.replace(' ', '_')
+    df.developer = df.developer.str.replace(' ','_')
+    df = df[df.average_forever > 0]
+#PUBLISHER ENCODING:
+    Publisher_list = ['']
+    word = ''
+    for x in df.publisher[(df.binned_hours == 'most_played') & (df.owners != '0 .. 20,000') & (df.owners != '20,000 .. 50,000') & (df.owners != '50,000 .. 100,000') & (df.owners != '100,000 .. 200,000') & (df.owners != '200,000 .. 500,000') & (df.owners != '500,000 .. 1,000,000')].tolist():
+        for a in str(x):
+            if a != ',':
+                word = word + a
+            else:
+                if word not in Publisher_list:
+                    Publisher_list.append(word)
+                    word = ''
+                word = ''
+        if word not in Publisher_list:
+            Publisher_list.append(word)
+            word = ''
+        word = ''
+    for x in Publisher_list:
+        x = x.replace(' ','_')
+    for x in Publisher_list:
+        df[f'Publisher_{x}'] = df.publisher.str.contains(x,regex = False)
+#Developer Encoding
+    Developer_list = ['']
+    word = ''
+    for x in df.developer[(df.binned_hours == 'most_played') & (df.owners != '0 .. 20,000') & (df.owners != '20,000 .. 50,000') & (df.owners != '50,000 .. 100,000') & (df.owners != '100,000 .. 200,000') & (df.owners != '200,000 .. 500,000') & (df.owners != '500,000 .. 1,000,000')].tolist():
+        for a in str(x):
+            if a != ',':
+                word = word + a
+            else:
+                if word not in Developer_list:
+                    Developer_list.append(word)
+                    word = ''
+                word = ''
+        if word not in Developer_list:
+            Developer_list.append(word)
+            word = ''
+        word = ''
+    for x in Developer_list:
+        x = x.replace(' ','_')
+    for x in Developer_list:
+        df[f'Developer_{x}'] = df.developer.str.contains(x,regex = False)
 #GENRE ENCODING:
 ### FINDS UNIQUE WORDS FROM LIST OF GENRES
-    '''genre_list = ['']
+    genre_list = []
     word = ''
-    for x in df.genres.tolist():
+    for x in df.genre.tolist():
         for a in str(x):
-            if a != '|':
+            if a != ',':
                 word = word + a
             else:
                 if word not in genre_list:
@@ -24,23 +84,20 @@ def clean_steamspy(df):
             genre_list.append(word)
             word = ''
         word = ''
-    genre_list.remove('')
     for x in genre_list:
-        genre_list.remove(x)
         x = x.replace(' ','_')
-        genre_list.append(x)
     for x in genre_list:
-        df[f'Genre_{x}'] = df.genres.str.contains(x)'''
-#Turning minutes to hours
-    df.iloc[:,[9,10,11,12]] = (df.iloc[:,[9,10,11,12]] / 120)
+        df[f'Genre_{x}'] = df.genre.str.contains(x)
+
 #Drops
-    df = df.drop(columns = ['score_rank','userscore','owners'])
+    df = df.drop(columns = ['score_rank','userscore'])
+    df = df.dropna()
     return df
 
 def scale_numeric(df):
     df.iloc[:,[6,7,8,9,10]] = scaler.fit_transform(df.iloc[:,[6,7,8,9,10]])
     return df
-def my_train_test_split(df, target):
+def my_train_test_split(df):
 
     train, test = train_test_split(df, test_size=.2, random_state=123)
     train, validate = train_test_split(train, test_size=.25, random_state=123)
